3418fc6423de4fb6fe97e0066fea7c0147ae6b6b
lluetolf <lukas.luetolf@gmail.com>
2019-11-24 12:37:45 +0100

    Add a side menu

--- canaweb-ui-app/src/app/app.component.ts
+++ canaweb-ui-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'canaweb-ui-app';
+  mode = new FormControl('over');
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 }
--- canaweb-ui-app/src/app/app.module.ts
+++ canaweb-ui-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,9 +18,12 @@ import { MaterialModule } from './shared/material.module';
   ],
   imports: [
     MaterialModule,
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
-    FieldsModule
+    FieldsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
