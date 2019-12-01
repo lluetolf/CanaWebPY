c34a30422b8144818563d897d51752e9eb2cb663
Lukas <lukas.luetolf@gmail.com>
2019-12-01 15:17:03 +0100

    Move routing of Field-List to Field module

--- canaweb-ui-app/src/app/app-routing.module.ts
+++ canaweb-ui-app/src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'fieldList', component: FieldListComponent },
   { path: 'payableList', component: FieldListComponent },
   { path: 'receivableList', component: FieldListComponent },
   { path: 'contact', component: FieldListComponent },
