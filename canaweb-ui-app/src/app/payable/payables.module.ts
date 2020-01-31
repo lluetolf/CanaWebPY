d86e174db337d9d352ff193fd80698e590205098
lluetolf <lukas.luetolf@gmail.com>
2020-01-31 16:38:33 +0100

    Fixing payable list bug

--- canaweb-ui-app/src/app/payable/payables.module.ts
+++ canaweb-ui-app/src/app/payable/payables.module.ts
@@ -5,13 +5,18 @@ import { PayablesRoutingModule } from './payables-routing.module';
 import { PayableListComponent } from './payable-list/payable-list.component';
 import { EditPayableComponent } from './edit-payable/edit-payable.component';
 import { CreatePayableComponent } from './create-payable/create-payable.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../shared/material.module';
 
 
 @NgModule({
   declarations: [PayableListComponent, EditPayableComponent, CreatePayableComponent],
   imports: [
     CommonModule,
-    PayablesRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    PayablesRoutingModule,
+    MaterialModule
   ]
 })
 export class PayablesModule { }
