411de34400a6ed9d3385738673f53a3e061b3cec
Lukas <lukas.luetolf@gmail.com>
2020-03-10 14:06:18 +0100

    Remove id to use _id 3

--- LogDecorator.py
+++ LogDecorator.py
@@ -1,21 +1,34 @@
 import logging
-import functools
+from functools import wraps, partial
 
 
-class LogDecorater(object):
-    def __init__(self):
-        self.logger = logging.getLogger('decorator-log')
+logger = logging.getLogger('decorator-log')
+logger.setLevel(logging.DEBUG)
 
-    def __call__(self, fn):
-        @functools.wraps(fn)
-        def decorated(*args, **kwargs):
-            try:
-                self.logger.debug("{0} - {1} - {2}".format(fn.__name__, args, kwargs))
-                result = fn(*args, **kwargs)
-                self.logger.debug(result)
-                return result
-            except Exception as ex:
-                self.logger.debug("Exception {0}".format(ex))
-                raise ex
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to cht
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+
+def LogDecorator(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        try:
+            logger.debug("{0} - {1} - {2}".format(fn.__name__, args, kwargs))
+            result = fn(*args, **kwargs)
+            logger.debug(result)
             return result
-        return decorated
+        except Exception as ex:
+            logger.debug("Exception {0}".format(ex))
+            raise ex
+        return result
+    return wrapper
