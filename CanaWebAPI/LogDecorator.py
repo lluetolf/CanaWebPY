1eeffbef6b81fb22c60332223998f288da2f5e80
Lukas <lukas.luetolf@gmail.com>
2020-03-12 14:05:59 +0100

    Change LogDecorator to use flask logger

--- CanaWebAPI/LogDecorator.py
+++ CanaWebAPI/LogDecorator.py
@@ -1,34 +1,17 @@
-import logging
+from flask import current_app as app
 from functools import wraps, partial
 
 
-logger = logging.getLogger('decorator-log')
-logger.setLevel(logging.DEBUG)
-
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# add formatter to cht
-ch.setFormatter(formatter)
-
-# add ch to logger
-logger.addHandler(ch)
-
-
-def LogDecorator(fn):
+def DebugLogs(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
         try:
-            logger.debug("{0} - {1} - {2}".format(fn.__name__, args, kwargs))
+            app.logger.debug("{0} - {1} - {2}".format(fn.__name__, args, kwargs))
             result = fn(*args, **kwargs)
-            logger.debug(result)
+            app.logger.debug("{}- {}".format(fn.__name__, result))
             return result
         except Exception as ex:
-            logger.debug("Exception {0}".format(ex))
+            app.logger.debug("Exception {0}".format(ex))
             raise ex
         return result
     return wrapper
