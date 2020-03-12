8119efdb9791d4cbf2e5b6c931ee070125d45a80
Lukas <lukas.luetolf@gmail.com>
2020-03-12 16:29:36 +0100

    Add Create payable including validation

--- CanaWebAPI/payable.py
+++ CanaWebAPI/payable.py
@@ -1,7 +1,9 @@
-from flask import (Blueprint, jsonify)
+from flask import (Blueprint, jsonify, request)
 from flask import current_app as app
 
 from CanaWebAPI.LogDecorator import DebugLogs
+from CanaWebAPI.entities.payable import validate_payable
+from CanaWebAPI.helper.InvalidUsage import InvalidUsage
 from CanaWebAPI.service.PayableRepository import PayableRepository
 
 bp = Blueprint('payables', __name__, url_prefix='/payable')
@@ -21,3 +23,37 @@ def get_all_payables() -> str:
     except Exception as e:
         app.logger.error("Failed: {}".format(repr(e)))
         return jsonify({"message": "Error fetchingall fields."}), 400
+
+
+@bp.route("/<payable_id>", methods=['GET'])
+@DebugLogs
+def get_payable(payable_id) -> str:
+    if not payable_id:
+        raise Exception("No valid payable_id provided.")
+
+    app.logger.info("Fetch payable with ObjectID: {}".format(payable_id))
+    try:
+        payable = PayableRepo.read_one(payable_id)
+        return jsonify(payable), 200
+    except Exception as e:
+        app.logger.error("Failed: {}".format(repr(e)))
+        return jsonify({"message": "Unable to find payable with id: {}".format(payable_id)}), 400
+
+
+@bp.route("", methods=['POST'])
+@DebugLogs
+def add_payable() -> str:
+    if not request.json:
+        raise Exception("No JSON message sent.")
+    try:
+        r = request.json
+        payable, errors = validate_payable(r)
+        if errors is not None:
+            app.logger.error(errors)
+            raise InvalidUsage(errors)
+        del r['_id']
+        payable = PayableRepo.create(payable)
+        return jsonify(payable), 200
+    except Exception as e:
+        app.logger.error("Failed: {}".format(repr(e)))
+        return jsonify({"message": "Error creating a new payable."}), 400
