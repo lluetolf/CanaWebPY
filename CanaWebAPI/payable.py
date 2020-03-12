29302ff93fd107dd43aefccbcd94722762d6fd2f
Lukas <lukas.luetolf@gmail.com>
2020-03-12 15:46:15 +0100

    Setup payables

--- CanaWebAPI/payable.py
+++ CanaWebAPI/payable.py
@@ -1,8 +1,23 @@
-import functools
+from flask import (Blueprint, jsonify)
+from flask import current_app as app
 
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
-from werkzeug.security import check_password_hash, generate_password_hash
+from CanaWebAPI.LogDecorator import DebugLogs
+from CanaWebAPI.service.PayableRepository import PayableRepository
 
 bp = Blueprint('payables', __name__, url_prefix='/payable')
+
+PayableRepo = PayableRepository()
+
+#
+# Field related endpoints
+# At some stage should be extracted to a Field Microservice
+#
+@bp.route("", methods=['GET'])
+@DebugLogs
+def get_all_payables() -> str:
+    try:
+        all_fields = PayableRepo.read_all()
+        return jsonify(all_fields), 200
+    except Exception as e:
+        app.logger.error("Failed: {}".format(repr(e)))
+        return jsonify({"message": "Error fetchingall fields."}), 400
