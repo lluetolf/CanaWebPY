8bf683b6d0efbcb1dc2030b8fbc8ad7b978006d8
Lukas <lukas.luetolf@gmail.com>
2020-03-12 14:44:01 +0100

    Fix some logger issues

--- CanaWebAPI/service/FieldRepository.py
+++ CanaWebAPI/service/FieldRepository.py
@@ -1,7 +1,7 @@
 from bson import ObjectId
 
-
 from CanaWebAPI import mongo
+from flask import current_app as app
 
 
 class FieldRepository(object):
@@ -20,17 +20,17 @@ class FieldRepository(object):
             raise Exception("Nothing to save, because field parameter is None")
 
     def read_one(self, field_id=None):
-        logger.debug("Read_One: {}".format(field_id))
+        app.logger.debug("Read_One: {}".format(field_id))
         return self.fields.find_one({"_id": ObjectId(field_id)})
 
     def update(self, field):
         if not field:
             raise Exception("Nothing to update, field is None")
 
-        logger.info("Updating field with ObjectId: {}".format(field['_id']))
+        app.logger.info("Updating field with ObjectId: {}".format(field['_id']))
         field['_id'] = ObjectId(field['_id'])
         result = self.fields.replace_one({'_id': field['_id']}, field)
-        logger.info("Return: {}".format(repr(result)))
+        app.logger.info("Return: {}".format(repr(result)))
         return bool(result.modified_count)
 
     def delete(self, field_id):
@@ -38,7 +38,7 @@ class FieldRepository(object):
             raise Exception("Nothing to delete, because field_id is None")
 
         result = self.fields.delete_one( {'_id': ObjectId(field_id) } )
-        logger.debug("Delete return: {}".format(repr(result)))
+        app.logger.debug("Delete return: {}".format(repr(result)))
 
         return bool(result.deleted_count)
 
