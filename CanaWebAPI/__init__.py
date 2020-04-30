9534abf5619c804d219a0b3eba6598041fd27fff
lluetolf <lukas.luetolf@gmail.com>
2020-04-30 12:17:23 +0200

    Divide config to DEV/TEST/PROD and define different MongoDB databases.

--- CanaWebAPI/__init__.py
+++ CanaWebAPI/__init__.py
@@ -11,30 +11,26 @@ mongo = PyMongo()
 logger = None
 
 
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
+    # ENV is set by the FLASK_ENV environment variable, which also sets DEBUG
+    app = Flask(__name__,
+                instance_relative_config=True,
+                template_folder="templates")
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+    if app.env == 'production':
+        from CanaWebAPI.config import ProdConfig
+        app.config.from_object(ProdConfig)
+    elif app.env == 'testing':
+        from CanaWebAPI.config import TestingConfig
+        app.config.from_object(TestingConfig)
     else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+        from CanaWebAPI.config import DevConfig
+        app.config.from_object(DevConfig)
 
     app.json_encoder = APIEncoder
     CORS(app)
 
-    app.config["MONGO_URI"] = os.getenv("CANAWEB_MONGO")
     mongo.init_app(app)
 
     #
---
+++ CanaWebAPI/config.py
@@ -0,0 +1,30 @@
+import os
+
+
+class Config:
+    """Base config vars."""
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SESSION_COOKIE_NAME = os.environ.get('SESSION_COOKIE_NAME')
+
+
+class ProdConfig(Config):
+    DEBUG = False
+    TESTING = False
+    MONGO_URI = 'mongodb+srv://canauser:{}@canawebmdb-p5yz3.azure.mongodb.net/CanaWebMDB?retryWrites=true&w=majority'.format(
+        os.environ.get('MONGO_PW', 'NO_PASSWORD_PROVIDED')
+    )
+
+
+class DevConfig(Config):
+    DEBUG = True
+    TESTING = True
+    MONGO_URI = 'mongodb+srv://canauser:{}@canawebmdb-p5yz3.azure.mongodb.net/CanaWebMDB_DEV?retryWrites=true&w=majority'.format(
+        os.environ.get('MONGO_PW', 'NO_PASSWORD_PROVIDED')
+    )
+
+
+class TestingConfig(Config):
+    TESTING = True
+    MONGO_URI = 'mongodb+srv://canauser:{}@canawebmdb-p5yz3.azure.mongodb.net/CanaWebMDB_TEST?retryWrites=true&w=majority'.format(
+        os.environ.get('MONGO_PW', 'NO_PASSWORD_PROVIDED')
+    )
