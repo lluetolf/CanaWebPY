2ff8790ae0f6b8f9299753ec37045896001d0801
lluetolf <lukas.luetolf@gmail.com>
2020-05-08 15:57:52 +0200

    Add tests for fields and switch queries from field_id to field_name.

--- CanaWebAPI/service/FieldRepository.py
+++ CanaWebAPI/service/FieldRepository.py
@@ -1,3 +1,5 @@
+import datetime
+
 from bson import ObjectId
 
 from CanaWebAPI import mongo
@@ -12,34 +14,35 @@ class FieldRepository(object):
 
     def create(self, field):
         if field is not None:
-            self.fields.insert(field)
+            self.fields.insert_one(field)
             print("Created new field with ObjectID: {}".format(field['_id']))
             return field
         else:
             raise Exception("Nothing to save, because field parameter is None")
 
-    def read_one(self, field_id=None):
-        app.logger.debug("Read_One: {}".format(field_id))
-        return self.fields.find_one({"_id": ObjectId(field_id)})
+    def read_one(self, field_name=None):
+        app.logger.debug("Read_One: {}".format(field_name))
+        field = self.fields.find_one({"name": field_name}, {'_id': 0})
+        return field
 
     def update(self, field):
         if not field:
             raise Exception("Nothing to update, field is None")
 
-        app.logger.info("Updating field with ObjectId: {}".format(field['_id']))
-        field['_id'] = ObjectId(field['_id'])
-        result = self.fields.replace_one({'_id': field['_id']}, field)
+        app.logger.info("Updating field with Name: {}".format(field['name']))
+        field['lastUpdated'] = datetime.datetime.now()
+        result = self.fields.replace_one({'name': field['name']}, field)
         app.logger.info("Return: {}".format(repr(result)))
         return bool(result.modified_count)
 
-    def delete(self, field_id):
-        if field_id is None:
-            raise Exception("Nothing to delete, because field_id is None")
+    def delete(self, field_name):
+        if field_name is None:
+            raise Exception("Nothing to delete, because field_name is None")
 
-        result = self.fields.delete_one( {'_id': ObjectId(field_id) } )
+        result = self.fields.delete_one({'name': field_name})
         app.logger.debug("Delete return: {}".format(repr(result)))
 
         return bool(result.deleted_count)
 
     def read_all(self):
-        return list(self.fields.find({}))
+        return list(self.fields.find({}, {'_id': 0}))
--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,7 +1,9 @@
 import json
+import time
 import unittest
 import datetime
 
+import dateutil
 from pymongo import MongoClient
 
 from tests.base import BaseTestCase
@@ -17,10 +19,14 @@ class FieldsBPTests(BaseTestCase):
             fields = client['CanaWebMDB_TEST']['fields']
             fields.delete_many({})
             base_data = [
-                {"name":"Apple","owner":"Elderberries","acquisitionDate":datetime.datetime(2018, 1, 1),"cultivatedArea":4,"ingenioId":-1,"lastUpdated":datetime.datetime.now(),"size":16},
-                {"name":"Banana","owner":"Figs","acquisitionDate":datetime.datetime(2018, 2, 2),"cultivatedArea":6,"ingenioId":-1,"lastUpdated":datetime.datetime.now(),"size":8},
-                {"name":"Clementine","owner":"Grapefruit","acquisitionDate":datetime.datetime(2018, 3, 3),"cultivatedArea":12,"ingenioId":-1,"lastUpdated":datetime.datetime.now(),"size":12},
-                {"name":"Durian","owner":"Honeydew Melon","acquisitionDate":datetime.datetime(2018, 4, 4),"cultivatedArea":10,"ingenioId":-1,"lastUpdated":datetime.datetime.now(),"size":10}]
+                {"name": "Apple", "owner": "Elderberries", "acquisitionDate": datetime.datetime(2018, 1, 1),
+                 "cultivatedArea": 4, "ingenioId": -1, "lastUpdated": datetime.datetime.now(), "size": 16},
+                {"name": "Banana", "owner": "Figs", "acquisitionDate": datetime.datetime(2018, 2, 2),
+                 "cultivatedArea": 6, "ingenioId": -1, "lastUpdated": datetime.datetime.now(), "size": 8},
+                {"name": "Clementine", "owner": "Grapefruit", "acquisitionDate": datetime.datetime(2018, 3, 3),
+                 "cultivatedArea": 12, "ingenioId": -1, "lastUpdated": datetime.datetime.now(), "size": 12},
+                {"name": "Durian", "owner": "Honeydew Melon", "acquisitionDate": datetime.datetime(2018, 4, 4),
+                 "cultivatedArea": 10, "ingenioId": -1, "lastUpdated": datetime.datetime.now(), "size": 10}]
             fields.insert_many(base_data)
         except Exception as e:
             print("Unable to prepare DB." + str(e))
@@ -41,7 +47,71 @@ class FieldsBPTests(BaseTestCase):
         response = self.client.get('/field')
         self.assert200(response)
         self.assertIsNotNone(response.json)
-        self.assertEqual(len(response.json), 4)
+        self.assertGreaterEqual(len(response.json), 2)
+
+    def test_add_field(self):
+        response = self.client.get('/field')
+        current_nbr_fields = len(response.json)
+        self.assert200(response)
+        self.assertIsNotNone(response.json)
+
+        new_field = {"name": "Jackfruit", "owner": "Kiwi", "acquisitionDate": datetime.datetime(2020, 3, 18),
+         "cultivatedArea": 5, "ingenioId": -1, "lastUpdated": datetime.datetime.now(), "size": 9}
+
+        response = self.client.post('/field', json=new_field)
+        self.assertEqual(response.status_code, 201)
+
+        response = self.client.get('/field')
+        self.assert200(response)
+        self.assertEqual(len(response.json), current_nbr_fields+1)
+
+    def test_pick_and_delete_field(self):
+        response = self.client.get('/field/Banana')
+        self.assert200(response)
+        self.assertIsNotNone(response.json)
+        banana = response.json
+
+        response = self.client.delete('/field/Banana')
+        self.assert200(response)
+        self.assertIsNotNone(response.json)
+
+        response = self.client.get('/field/Banana')
+        self.assertEqual(response.status_code, 204)
+
+        response = self.client.post('/field', json=banana)
+        self.assertEqual(response.status_code, 201)
+
+        response = self.client.get('/field/Banana')
+        self.assert200(response)
+        self.assertIsNotNone(response.json)
+
+    def test_update_field(self):
+        response = self.client.get('/field/Durian')
+        self.assert200(response)
+        self.assertIsNotNone(response.json)
+
+        new_durian = {
+            "name": "Durian",
+            "owner": "Honeydew Melon2",
+            "acquisitionDate": datetime.datetime(2016, 6, 6),
+            "cultivatedArea": 6,
+            "ingenioId": 6,
+            "lastUpdated": datetime.datetime.now() - datetime.timedelta(seconds=10),
+            "size": 6
+        }
+
+        response = self.client.patch('/field', json=new_durian)
+        response = self.client.get('/field/Durian')
+        self.assert200(response)
+        self.assertIsNotNone(response.json)
+        fetched_durian = response.json
+        fetched_durian['acquisitionDate'] = dateutil.parser.parse(fetched_durian['acquisitionDate'])
+        fetched_durian['lastUpdated'] = dateutil.parser.parse(fetched_durian['lastUpdated'])
+        for i in new_durian.keys():
+            if i != 'lastUpdated':
+                self.assertEqual(new_durian[i], fetched_durian[i])
+            else:
+                self.assertNotEqual(new_durian[i], fetched_durian[i])
 
 
 if __name__ == "__main__":
