52ba9f0247cec913230675d2e9eca1c750b5f12f
lluetolf <lukas.luetolf@gmail.com>
2020-05-08 17:56:24 +0200

    Cosmetic

--- CanaWebAPI/views/LogDecorator.py
+++ CanaWebAPI/views/LogDecorator.py
@@ -8,7 +8,7 @@ def DebugLogs(fn):
         try:
             app.logger.debug("{0} - {1} - {2}".format(fn.__name__, args, kwargs))
             result = fn(*args, **kwargs)
-            app.logger.debug("{}- {}".format(fn.__name__, result))
+            app.logger.debug("{} - {}".format(fn.__name__, result))
             return result
         except Exception as ex:
             app.logger.debug("Exception {0}".format(ex))
