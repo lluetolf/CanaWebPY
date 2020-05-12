f3afc7415a725f3442b979c2e174f3932e9323e3
lluetolf <lukas.luetolf@gmail.com>
2020-05-12 12:20:47 +0200

    Move Payables from Classes to dict using schema. Align Payable and Field.

--- CanaWebAPI/service/PayableRepository.py
+++ CanaWebAPI/service/PayableRepository.py
@@ -1,9 +1,9 @@
+import datetime
+
 from bson import ObjectId
 
 from CanaWebAPI import mongo
 from flask import current_app as app
-
-from CanaWebAPI.entities.payable_entity import PayableEntity
 
 
 class PayableRepository(object):
@@ -13,17 +13,18 @@ class PayableRepository(object):
         self.payables = mongo.db.payables
 
     def read_all(self) -> []:
+        app.logger.debug("Read_All: {}")
         return list(self.payables.find({}))
 
-    def read_one(self, payable_id=None) -> {}:
+    def read_one(self, payable_id: str) -> {}:
         app.logger.debug("Read_One: {}".format(payable_id))
         return self.payables.find_one({"_id": ObjectId(payable_id)})
 
-    def create(self, payable: PayableEntity)-> bool:
+    def create(self, payable: {}) -> bool:
         if payable is not None:
             # Make sure _id is not provided
-            payable.content().pop('_id', None)
-            result = self.payables.insert_one(payable.content())
+            payable.pop('_id', None)
+            result = self.payables.insert_one(payable)
             if result.inserted_id is not None:
                 app.logger.info("Created new Payable with ObjectID: {}".format(result.inserted_id))
                 return True
@@ -33,20 +34,18 @@ class PayableRepository(object):
         else:
             raise Exception("Nothing to save, because field parameter is None")
 
-    def update(self, payable: PayableEntity) -> bool:
-        if payable is None:
+    def update(self, payable: {}) -> bool:
+        if payable is None and payable.get('_id', 'UNKNOWN') != 'UNKNOWN':
             raise Exception("Nothing to update, payable is None")
 
-        app.logger.info("Updating payable with ObjectId: {}".format(payable._id))
-        result = self.payables.replace_one({'_id': payable._id}, payable)
-        app.logger.info("Return: {}".format(repr(result)))
+        app.logger.info("Updating payable with ObjectId: {}".format(payable['_id']))
+        payable['lastUpdated'] = datetime.datetime.now()
+        result = self.payables.replace_one({'_id': ObjectId(payable['_id'])}, payable)
         return bool(result.modified_count)
 
-    def delete(self, payable_id):
+    def delete(self, payable_id: str):
         if payable_id is None:
             raise Exception("Nothing to delete, because payable_id is None")
 
         result = self.payables.delete_one({'_id': ObjectId(payable_id)})
-        app.logger.debug("Delete return: {}".format(repr(result)))
-
         return bool(result.deleted_count)
--- CanaWebAPI/views/field.py
+++ CanaWebAPI/views/field.py
@@ -1,7 +1,7 @@
 from flask import (Blueprint, jsonify, request)
 from flask import current_app as app
 
-from CanaWebAPI.entities.entity_creator import check_field
+from CanaWebAPI.entities.entity_checks import check_field
 from CanaWebAPI.views.LogDecorator import DebugLogs
 from CanaWebAPI.service.FieldRepository import FieldRepository
 from CanaWebAPI.views.auth import token_required
@@ -24,7 +24,7 @@ def get_all_fields(current_user) -> str:
         return jsonify(all_fields), 200
     except Exception as e:
         app.logger.error("Failed: {}".format(e.details))
-        return respond_failed('Error fetching all fields.', response_code=500)
+        return respond_failed("Request failed internally. Check logs."), 500
 
 
 @bp.route("/<field_name>", methods=['GET'])
@@ -37,19 +37,19 @@ def get_field(current_user, field_name) -> str:
     try:
         field = FieldRepo.read_one(field_name)
         if field is None:
-            return {}, 204
-
-        return jsonify(field), 200
+            return jsonify({}), 204
+        else:
+            return jsonify(field), 200
     except Exception as e:
         app.logger.error("Failed: {}".format(e.details))
-        return jsonify({"message": "Unable to find field with Name: {}".format(field_name)}), 400
+        return respond_failed("Request failed internally. Check logs."), 500
 
 
 @bp.route("", methods=['POST'])
 @token_required
 @DebugLogs
 def add_field(current_user) -> str:
-    if not request.json:
+    if not request.is_json:
         return respond_failed("No JSON message sent.")
 
     field, error = check_field(request.json)
@@ -64,7 +64,7 @@ def add_field(current_user) -> str:
 
     except Exception as e:
         app.logger.error("Failed: {}".format(e.details))
-        return respond_failed('Error creating a new field.', response_code=500)
+        return respond_failed("Request failed internally. Check logs."), 500
 
 
 @bp.route("", methods=['PATCH'])
@@ -85,7 +85,7 @@ def update_field(current_user) -> str:
             return respond_failed('Issues connecting to the DB.', response_code=500)
     except Exception as e:
         app.logger.error("Failed to update: {}".format(e.details))
-        return respond_failed('Error updating a new field.', response_code=500)
+        return respond_failed("Request failed internally. Check logs."), 500
 
 
 @bp.route("/<field_name>", methods=['DELETE'])
@@ -95,7 +95,6 @@ def delete_field(current_user, field_name) -> str:
     if not field_name:
         return respond_failed("No Name provided")
 
-    app.logger.info("Delete field with Name: {}".format(field_name))
     try:
         if FieldRepo.delete(field_name):
             return respond_success("Deleted")
@@ -103,9 +102,12 @@ def delete_field(current_user, field_name) -> str:
             return respond_failed('Unable to delete field with id: {}'.format(field_name))
     except Exception as e:
         app.logger.error("Failed to delete: {}".format(e.details))
-        return respond_failed('Error deleting a field.', response_code=500)
+        return respond_failed("Request failed internally. Check logs."), 500
 
 
+#
+# Helper Functions
+#
 def respond_failed(msg, response_code=400):
     app.logger.info("No Name provided")
     return jsonify({'status': 'failed', 'message': msg}), response_code
